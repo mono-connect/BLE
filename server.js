6c5f793074a36273ef39e3f8d94b46d07873904e
佐久間大輔 <sakumadaisuke@sakumadaisukenoiMac.local>
2018-06-14 07:57:19 +0900

    0614

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ exports.handler = function(event, context, callback) {
         //ビーコンMACが登録されているかチェック
         var sensor_macaddrs = list[1];
         var selectQuery = 'SELECT * FROM ?? where sensor_macaddrs IN (?)';
-        connection.query(selectQuery, [ 'master', sensor_macaddrs ], function(err, rows, fields) {
+        connection.query(selectQuery, [ 'sample_table', sensor_macaddrs ], function(err, rows, fields) {
             if(err) {
                 console.log(formatted + 'Error1');
                 return;
@@ -79,88 +79,76 @@ exports.handler = function(event, context, callback) {
             console.log(formatted + ' sensor type = ' + rows[0].sensor_type);
             
             //センサー共通処理
-            var box_name = rows[0].box_name;
-            var box_beacon_id = rows[0].box_beacon_id;
-            var box_style = rows[0].box_style;
-            var sensor_type = rows[0].sensor_type;
-            var sensor_tenant_id = rows[0].sensor_tenant_id;
-            var tenant_name = rows[0].tenant_name;
-            var gatewaymac = list[2];
             var rssi = list[3];
             var payload = hexBufferReverse(list[4]);
             var battery = parseInt(payload.slice(22,26), 16)/100;
             var date = moment(list[5],'X').format("YYYY-MM-DD HH:mm:ss");
             var hour_minutes = moment(list[5],'X').format("HH:mm");
-            
-            var inseartQuery = 'INSERT INTO sample_table set ?';
-            
-            //マグネットセンサー処理
-            if(sensor_type == "MAGNET") {
-                var status = parseInt(payload.slice(20,22));
-                
-                if(status == 4) {
-                    connection.query(inseartQuery, 
-                    {box_status:'CLOSE',last_status_confirmed_at:date,hour_minutes:hour_minutes,status_changed_at:date,box_name:box_name,box_beacon_id:box_beacon_id,
-                    box_style:box_style,sensor_macaddrs:sensor_macaddrs,sensor_type:sensor_type,sensor_battery:battery,sensor_tenant_id:sensor_tenant_id,tenant_name:tenant_name}, 
-                    (err, res)  => {
-                        if (err) throw err;
-                        console.log(formatted + ' mysql insert OK');
-                    });
-                }
-                if(status == 0) {
-                    connection.query(inseartQuery, 
-                    {box_status:'OPEN',last_status_confirmed_at:date,hour_minutes:hour_minutes,status_changed_at:date,box_name:box_name,box_beacon_id:box_beacon_id,
-                    box_style:box_style,sensor_macaddrs:sensor_macaddrs,sensor_type:sensor_type,sensor_battery:battery,sensor_tenant_id:sensor_tenant_id,tenant_name:tenant_name}, 
-                    (err, res)  => {
-                        if (err) throw err;
-                        console.log(formatted + ' mysql insert OK');
-                    });
-                }
-            }
+            var last_status = rows[0].last_status;
+            var sensor_type = rows[0].sensor_type;
+            var status = parseInt(payload.slice(20,22));
+            var temp = parseInt(payload.slice(16,20), 16)/100;
+            var humid = parseInt(payload.slice(12,16), 16)/100;
 
-            //遮断、人感、マット、加速度(衝撃)センサー処理
-            if(sensor_type == "INTERCEPTION" || sensor_type == "HUMAN" || sensor_type == "MATT" || sensor_type == "GRAVITY") {
-                var status = parseInt(payload.slice(20,22));
-                
-                if(status == 4 || status == 2) {
-                    connection.query(inseartQuery, 
-                    {box_status:'ACTIVE',last_status_confirmed_at:date,hour_minutes:hour_minutes,status_changed_at:date,box_name:box_name,box_beacon_id:box_beacon_id,
-                    box_style:box_style,sensor_macaddrs:sensor_macaddrs,sensor_type:sensor_type,sensor_battery:battery,sensor_tenant_id:sensor_tenant_id,tenant_name:tenant_name}, 
-                    (err, res)  => {
-                        if (err) throw err;
-                        console.log(formatted + ' mysql insert OK');
-                    });
+            //SQL共通処理
+            var updateQuery = 'UPDATE sample_table SET ?? = ? WHERE sensor_macaddrs = ?';
+            var obj1 = { 'last_status_confirmed_at':date, 'hour_minutes':hour_minutes, 'sensor_battery': battery };
+            var obj2 = { 'last_status_confirmed_at':date, 'hour_minutes':hour_minutes, 'sensor_battery': battery, 'box_status': 'ACTIVE', 'last_status': status, 'status_changed_at': date };
+            var obj3 = { 'last_status_confirmed_at':date, 'hour_minutes':hour_minutes, 'sensor_battery': battery, 'box_status': 'INACTIVE', 'last_status': status, 'status_changed_at': date };
+            var obj4 = { 'last_status_confirmed_at':date, 'hour_minutes':hour_minutes, 'sensor_battery': battery, 'temp': temp, 'humidity': humid, 'status_changed_at': date };
+            
+            //マグネットセンサー、遮断、人感、マット、加速度(衝撃)センサー処理処理
+            if(sensor_type == "MAGNET" || sensor_type == "INTERCEPTION" || sensor_type == "HUMAN" || sensor_type == "MATT" || sensor_type == "GRAVITY" ) { 
+                if(status == last_status) {
+                    Object.keys(obj1).forEach(function(key) {
+                        var val = this[key];
+                        connection.query(updateQuery, [key, val, sensor_macaddrs],
+                            (err, res)  => {
+                                if (err) throw err;
+                                console.log(formatted + key +":" +val + 'update OK');
+                            }
+                        );
+                    }, obj1);
                 }
-                if(status == 0) {
-                    connection.query(inseartQuery, 
-                    {box_status:'INACTIVE',last_status_confirmed_at:date,hour_minutes:hour_minutes,status_changed_at:date,box_name:box_name,box_beacon_id:box_beacon_id,
-                    box_style:box_style,sensor_macaddrs:sensor_macaddrs,sensor_type:sensor_type,sensor_battery:battery,sensor_tenant_id:sensor_tenant_id,tenant_name:tenant_name}, 
-                    (err, res)  => {
-                        if (err) throw err;
-                        console.log(formatted + ' mysql insert OK');
-                    });
+                else if(status == 4 || status == 2) {
+                    Object.keys(obj2).forEach(function(key) {
+                        var val = this[key];
+                        connection.query(updateQuery, [key, val, sensor_macaddrs],
+                            (err, res)  => {
+                                if (err) throw err;
+                                console.log(formatted + key +":" +val + 'update OK');
+                            }
+                        );
+                    }, obj2);
+                }
+                else if(status == 0) {
+                    Object.keys(obj3).forEach(function(key) {
+                        var val = this[key];
+                        connection.query(updateQuery, [key, val, sensor_macaddrs],
+                            (err, res)  => {
+                                if (err) throw err;
+                                console.log(formatted + key +":" +val + 'update OK');
+                            }
+                        );
+                    }, obj3);
                 }
             }
 
             //温度・湿度センサー処理
             if(sensor_type == "TEMP") {
-                var temp = parseInt(payload.slice(16,20), 16)/100;
-                var humid = parseInt(payload.slice(12,16), 16)/100;
-                console.log(formatted + ' battery:' + battery);
-                console.log(formatted + ' humidity:' + humid);
-                console.log(formatted + ' temp:' + temp);
-                console.log(formatted + ' timestamp:' + date);
-                connection.query(inseartQuery, 
-                {box_status:'NULL',last_status_confirmed_at:date,hour_minutes:hour_minutes,status_changed_at:date,box_name:'NULL',box_beacon_id:"1",box_style:'NULL',
-                sensor_macaddrs:sensor_macaddrs,sensor_type:sensor_type,sensor_battery:battery,sensor_tenant_id:sensor_tenant_id,tenant_name:'NULL',temp:temp,humidity:humid}, 
-                (err, res)  => {
-                if (err) throw err;
-                    console.log(formatted + 'mysql insert OK');
-                });
+                Object.keys(obj4).forEach(function(key) {
+                    var val = this[key];
+                    connection.query(updateQuery, [key, val, sensor_macaddrs],
+                        (err, res)  => {
+                            if (err) throw err;
+                            console.log(formatted + key +":" +val + 'update OK');
+                        }
+                    );
+                }, obj4);
             }
         });
     });
-} 
+}
 
 //エンディアン変換関数
 function hexBufferReverse (text) {
